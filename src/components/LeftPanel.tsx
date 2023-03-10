a73af8b3a7ac31febb4c61ac697e360d87b40019
Zak Zangrando Cardoso <zakzangrandocardoso@alexandria.home>
2023-03-10 19:00:38 +0100

    Adds arrow open and close on the left side

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import { Col, Button } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import { BsArrowLeftCircle } from "react-icons/bs";
 // import styles from "../styles/leftPanelStyle.module.css";
 
 interface myNotesInterface {
@@ -17,10 +18,65 @@ export default function LeftPanel({ isOpenLeft, setIsOpenLeft }): JSX.Element {
     { noteName: "note 2", noteNumber: 14343 },
     { noteName: "note 3", noteNumber: 22343 },
   ];
+  const ArrowLeft = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="bi bi-arrow-left"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+      />
+    </svg>
+  );
+  
+  const ArrowRight = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-arrow-right"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+      />
+    </svg>
+  );
+
+  const OpenCloseSideTabButton = () => (
+    <Row>
+      {isOpenLeft ? (
+        <>
+          <Col></Col>
+          <Col></Col>
+          <Col>
+            <Button variant="light" onClick={() => setIsOpenLeft(!isOpenLeft)}>
+              <ArrowLeft />
+            </Button>
+          </Col>
+        </>
+      ) : (
+        <>
+          <Col>
+            <Button variant="light" onClick={() => setIsOpenLeft(!isOpenLeft)}>
+              <ArrowRight />
+            </Button>
+          </Col>
+        </>
+      )}
+    </Row>
+  );
 
   return (
     <Col>
-      <Button onClick={() => setIsOpenLeft(!isOpenLeft)}>View Notes</Button>
+      <OpenCloseSideTabButton />
       {isOpenLeft ? (
         <>
           <h1>{t("yourNotes")}</h1>
@@ -34,7 +90,8 @@ export default function LeftPanel({ isOpenLeft, setIsOpenLeft }): JSX.Element {
           </ul>
         </>
       ) : (
-        <h2>closed</h2>
+        <></>
       )}
     </Col>
-  );}
+  );
+}
