68adb571060df3abf0d3e3a0df2b50ce79de43cb
Zak Zangrando Cardoso <zakzangrandocardoso@alexandria.home>
2023-03-19 21:16:49 +0100

    Small change to svg and preparation for the text input component

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -5,36 +5,50 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 //
 import Indent from "./svg/Indent";
 import Unindent from "./svg/Unindent";
-import Check from "./svg/Check"
+import Check from "./svg/Check";
 // import styles from "../../Styles/LogInPageStyle.css";
 
 export default function TextArea(): JSX.Element {
   const [inputText, setInputText] = useState("");
-  const [indent, setIndent] = useState(false)
-  const [unindent, setUnindent] = useState(false)
+  // const [indent, setIndent] = useState("0");
   // const { t } = useTranslation();
   console.log("Input: ", inputText);
+
+  const confirmNoteEntry = () => {
+    const textToInput = inputText
+    setInputText("")
+    console.log(textToInput)
+  };
+
+  const setIndentationLevel = (level) => {
+if (level === true) {
+  /// add indentation
+  console.log("plus")
+} else {
+  // remove indentation
+  console.log("minus")
+}
+  }
 
   const IndentationArea = () => (
     <Row>
       <Col>
-        <Button variant="flat" size="lg" onClick={() => setIndent(!indent)}>
+        <Button variant="flat" size="lg" onClick={() => setIndentationLevel(true)}>
           <Unindent />
         </Button>
       </Col>
       <Col>
-        <Button variant="flat" size="lg" onClick={() => setUnindent(!unindent)}>
+        <Button variant="flat" size="lg" onClick={() => setIndentationLevel(false)}>
           <Indent />
         </Button>
       </Col>
       <Col>
-        <Button variant="flat" size="lg" onClick={() => setUnindent(!unindent)}>
+        <Button variant="flat" size="lg" onClick={() => confirmNoteEntry()}>
           <Check />
         </Button>
       </Col>
     </Row>
   );
-
 
   return (
     <Container>
@@ -49,7 +63,7 @@ export default function TextArea(): JSX.Element {
           />
         </Col>
       </Row>
-      <br/>
+      <br />
       <IndentationArea />
     </Container>
   );
