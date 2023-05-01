ec3127b1d85dcdc8f7e8c87d3cc4c00a61b55182
zakzc <zak.zangrando@outlook.com>
2023-05-01 21:00:51 +0200

    Initiates log in and sign up functionalities

---
+++ src/components/LogInSignUp/LogInSignUpPage.tsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+///
+import { Form, Button } from "react-bootstrap";
+
+interface Props {}
+
+const LogInSignUpPage: React.FC<Props> = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [errorOnCredentials, setErrorOnCredentials] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (isSignUp && password !== confirmPassword) {
+      setPasswordsMatch(false);
+      return;
+    } else {
+      const data = { email, password, requestType: isSignUp };
+
+      try {
+        console.log("Will send: ", email, password);
+        const res = await fetch("api/user", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        const result = await res.json();
+        if (
+          (result.result === "success" &&
+            result.process === "user is logged in") ||
+          result.process === "user registered"
+        ) {
+          router.push("/");
+        } else if (result.data === false) {
+          setErrorOnCredentials("Error on user credentials, please try again.");
+        }
+        console.log("Return is: ", result);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+      return;
+    }
+  };
+
+  const handleSwitch = () => {
+    setIsSignUp((prev) => !prev);
+    setPasswordsMatch(true);
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <br />
+      <Form onSubmit={handleSubmit}>
+        <h1 className="text-center mb-3">{isSignUp ? "Sign Up" : "Log In"}</h1>
+        <br />
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <br />
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <br />
+        {isSignUp && (
+          <Form.Group controlId="formBasicConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              style={{
+                border: passwordsMatch ? "1px solid #ced4da" : "1px solid red",
+              }}
+            />
+            {!passwordsMatch && (
+              <Form.Text className="text-danger">
+                Passwords do not match.
+              </Form.Text>
+            )}
+            <br /> <br />
+          </Form.Group>
+        )}
+        <Button variant="primary" type="submit">
+          {isSignUp ? "Sign Up" : "Log In"}
+        </Button>
+        <br /> <br />
+        <Form.Text className="text-muted mt-2">
+          {isSignUp ? "Already have an account? " : "Don't have an account? "}
+
+          <Button
+            variant="link"
+            className="p-0 ml-1"
+            onClick={handleSwitch}
+            style={{ fontWeight: "bold" }}
+          >
+            {isSignUp ? "Log In" : "Sign Up"}
+          </Button>
+          <br />
+          <h6 style={{ backgroundColor: "yellow" }}> {errorOnCredentials}</h6>
+          <br />
+        </Form.Text>
+      </Form>
+    </div>
+  );
+};
+
+export default LogInSignUpPage;
--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -13,27 +13,57 @@ function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     const email = req.body.email;
     const user = req.body.user;
-    console.log("Log in for: ", email, user);
-
-    res.status(200).json({ result: "success", data: "user is logged in" });
+    res
+      .status(200)
+      .json({ result: "success", data: "API is operational and working" });
   }
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log("USER - Log in for: ", email, password);
   if (req.method === "POST") {
-    const email = req.body.email;
-    const password = req.body.password;
-    console.log("USER - Log in for: ", email, password);
-    const registerUser: RegisterUser = {
-      id: new Date().toISOString() + "-" + uuid(),
-      email,
-      password,
-    };
-    // store data locally
-    const filePath = path.join(process.cwd(), "data", "users.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
-    data.push(registerUser);
-    fs.writeFileSync(filePath, JSON.stringify(data));
-    // response
-    res.status(201).json({ result: "success", data: registerUser });
+    // Sign up request
+    if (req.body.requestType === true) {
+      const registerUser: RegisterUser = {
+        id: new Date().toISOString() + "-" + uuid(),
+        email,
+        password,
+      };
+      // store data locally
+      const filePath = path.join(process.cwd(), "src/data", "users.json");
+      const fileData = fs.readFileSync(filePath);
+      const data = JSON.parse(fileData);
+      data.push(registerUser);
+      fs.writeFileSync(filePath, JSON.stringify(data));
+      // response
+      res
+        .status(201)
+        .json({
+          result: "success",
+          process: "user registered",
+          data: registerUser,
+        });
+    } else {
+      // Log in request
+      // store data locally
+      const filePath = path.join(process.cwd(), "src/data", "users.json");
+      const fileData = fs.readFileSync(filePath);
+      const data = JSON.parse(fileData);
+      const user = data.find(
+        (u) => u.email === email && u.password === password
+      );
+
+      if (user) {
+        res.status(200).json({
+          result: "success",
+          process: "user is logged in",
+          data: true,
+        });
+      } else {
+        res
+          .status(401)
+          .json({ success: true, process: "User is not logged in", data: false });
+      }
+    }
   }
 }
 
